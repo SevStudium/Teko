b40b614a542a1e5edf435eae78f1f09d4fb2921d
DESKTOP-SG\sever <severin.gschwind@edu.teko.ch>
2025-09-03 20:26:38 +0200

    Erster versuch bei SRF

--- Webscrapping_SG_001.py
+++ Webscrapping_SG_001.py
@@ -2,8 +2,28 @@
 #Praxisarbeit Webautomatisierung
 
 
+# Falls noch nicht vorhanden:
+# pip install requests beautifulsoup4
 
-print("Hello Switzerland")
 
+print("Hello")
+
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://www.srf.ch/news"
+
+
+antwort = requests.get(url)
+html_code = antwort.text
+
+soup = BeautifulSoup(html_code, "html.parser")
+
+headlines = soup.find_all("h2")
+
+for h in headlines:
+    text = h.get_text(strip=True)
+    if len(text) > 5:  # ganz kurze Sachen ignorieren
+        print(text)
 
 
